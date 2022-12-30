64afb58ff74cac218596404b113b6fdf3294fbd2
raj <raj.singh@impelsys.com>
2022-12-30 16:20:35 +0530

    second commit

---
+++ src/data/product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "The Castle",
+        description: "Franz Kafka",
+        price: 350,
+        currency: "$",
+        thumb: "./images/1.jfif"
+    },
+    {
+        id: 2,
+        product_name: "Mrs. Dollaway",
+        description: "Virginia Woolf",
+        price: 250,
+        currency: "$",
+        thumb: "./images/2.jfif"
+    },
+    {
+        id: 3,
+        product_name: "Dubliner",
+        description: "James Joyce",
+        price: 350,
+        currency: "$",
+        thumb: "./images/3.jfif"
+    },
+    {
+        id: 4,
+        product_name: "Sucide",
+        description: "Emile Durkhiem",
+        price: 350,
+        currency: "$",
+        thumb: "./images/4.jfif"
+    },
+    {
+        id: 5,
+        product_name: "Democracy in America",
+        description: "Alexis de Tocqueville",
+        price: 250,
+        currency: "$",
+        thumb: "./images/5.jpg"
+    },
+    {
+        id: 6,
+        product_name: "The age of capitalism",
+        description: "Max Weber",
+        price: 350,
+        currency: "$",
+        thumb: "./images/6.jfif"
+    },
+    {
+        id: 7,
+        product_name: "One hundred year of solitude",
+        description: "Gabriel Garcia Marquez",
+        price: 350,
+        currency: "$",
+        thumb: "./images/7.jfif"
+    },
+    {
+        id: 8,
+        product_name: "My India",
+        description: "Jim Corbett",
+        price: 250,
+        currency: "$",
+        thumb: "./images/8.jfif"
+    },
+    {
+        id: 9,
+        product_name: "Sapiens",
+        description: "Yuval Noah Harari",
+        price: 350,
+        currency: "$",
+        thumb: "./images/9.png"
+    }
+]
+export default product_card;
